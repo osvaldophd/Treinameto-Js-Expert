69db0cdc5a2b324bc3310440d474dc5450311582
osvaldo.avelino <osvaldo.avelino@jupiter.co.ao>
2023-10-26 14:55:10 +0100

    terminado a aula 01 de mocks

--- JS Expert/modulo01/auala01-mocks/index.test.js
+++ JS Expert/modulo01/auala01-mocks/index.test.js
@@ -12,4 +12,44 @@ const assert = require('assert');
 
     await assert.rejects(result, expected);
   }
+  {
+    const filePath = './mocks/invalid-header.csv'
+    const expected = new Error(ErrorContant.FILE_FIELDS_ERROR_MENSAGE)
+    const result = File.csvToJson(filePath)
+
+    await assert.rejects(result, expected);
+  }
+  {
+    const filePath = './mocks/fiveitems-invalid.csv'
+    const expected = new Error(ErrorContant.FILE_LENGTH_ERROR_MENSAGE)
+    const result = File.csvToJson(filePath)
+
+    await assert.rejects(result, expected);
+  }
+  {
+    const filePath = './mocks/treetems-valid.csv'
+    const expected = [{
+      id: 1,
+      name: 'Osvaldo Avelino',
+      profission: 'developer',
+      age: 35
+    },
+      {
+        id: 2,
+        name: 'Aldair Avelino',
+        profission: 'student',
+        age: 19
+      },
+      {
+        id: 3,
+        name: 'Dominga Avelino',
+        profission: 'norse',
+        age: 31
+      }
+    ]
+
+   const result = await File.csvToJson(filePath)
+
+   //  assert.deepEqual(result, expected);
+  }
 })()
--- JS Expert/modulo01/auala01-mocks/src/file.js
+++ JS Expert/modulo01/auala01-mocks/src/file.js
@@ -9,23 +9,58 @@ const DEFAULT_OPTIONS = {
 class File {
   static async csvToJson(filePath) {
     const content = await readFile(filePath, 'utf8')
-   const validation =  this.isValid(content)
+    const validation = this.isValid(content)
 
-    if(!validation.valid) throw new Error(validation.error);
+    if (!validation.valid) throw new Error(validation.error);
+
+    const result = this.csvToJson(content);
+
+    return result
   }
 
   static isValid(csvString, options = DEFAULT_OPTIONS) {
     const [headers, ...fileWithoutHeaders] = csvString.split(/\r?\n/).filter(item => item !== '');
+    const isHadersValid = headers === DEFAULT_OPTIONS.fields.join(',')
 
-    console.log(headers)
-    console.log(fileWithoutHeaders)
 
-    if (!fileWithoutHeaders.length) {
+    if (!isHadersValid) {
+      return {
+        error: ErrorContant.FILE_FIELDS_ERROR_MENSAGE,
+        valid: false
+      }
+    }
+
+    if (!fileWithoutHeaders.length || fileWithoutHeaders.length > DEFAULT_OPTIONS.maxLLines) {
       return {
         error: ErrorContant.FILE_LENGTH_ERROR_MENSAGE,
         valid: false
       }
     }
+
+    return {valid: true}
+
+  }
+
+  static perseCSVToJson(csvString) {
+    const lines = csvString.split(/\r?\n/).filter(item => item !== '');
+    const fistLine = lines.shift();
+    const header = fistLine.split(',');
+
+    const users = lines.map(line => {
+      const columns = line.split(',')
+      const user = {}
+
+      for (const index in columns) {
+        user[header[index]] = columns[index].trim()
+      }
+
+      return user;
+    })
+
+    console.log(users)
+    console.log(header)
+    return users
+
   }
 }
 
