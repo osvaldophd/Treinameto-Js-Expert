a5fa1a1013ccb569314b9bec28b8208f8e8a6f81
osvaldo <phdosvaldoavelino@gmail.com>
2023-11-16 21:50:41 +0100

    Ajuste

--- JS Expert/modulo01/aula02-stubs/src/service.test.js
+++ JS Expert/modulo01/aula02-stubs/src/service.test.js
@@ -1,13 +1,39 @@
 const Service = require('./service');
 const assert = require('assert');
+const {createSandbox} = require('sinon');
+const sinon = createSandbox();
+
 const BASE_URL_1 = 'https://swapi.dev/api/planets/1';
 const BASE_URL_2 = 'https://swapi.dev/api/planets/2';
+const mocks = {
+    alderaan: require('../mocks/alderaan.json'),
+    tatooine: require('../mocks/tatooine.json'),
+};
 
 ;(async () => {
-  {
-   const service = new Service();
-   const dados = await service.makeRequest(BASE_URL_1);
+    const stub = sinon.stub(
+        Service,
+        Service.makeRequest.name
+    );
+    /*  {
+          //vai para internet⁄⁄⁄
+       const service = new Service();
+       const dados = await service.makeRequest(BASE_URL_2);
 
-   console.log('dados',JSON.stringify(dados));
-  }
+       console.log('dados',JSON.stringify(dados));
+      }*/
+    {
+        stub
+            .withargs(BASE_URL_1)
+            .resolves(mocks.tatooine);
+        stub
+            .withargs(BASE_URL_2)
+            .resolves(mocks.alderaan);
+
+        const expected ={
+            name: "Tatooine",
+            surfaceWater: "1",
+            appeardIn: 5
+        }
+    }
 })();
